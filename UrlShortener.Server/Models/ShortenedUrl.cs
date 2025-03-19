8e73f79fce4d3fd9addce9daace846fb1acc6be3
Mykhailo Bublyk <shwbaker@gmail.com>
2025-03-19 14:51:58 +0200

    Добавьте файлы проекта.

---
+++ UrlShortener.Server/Models/ShortenedUrl.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Server.Models;
+
+public class ShortenedUrl
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [Url]
+    public string Url { get; set; }
+    
+    [Required]
+    public string ShortCode { get; set; }
+    
+    public int CreatedById { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public int Clicks { get; set; }
+    
+    public virtual User CreatedBy { get; set; }
+}
---
+++ UrlShortener.Server/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Server.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string Username { get; set; }
+    
+    [Required]
+    public string PasswordHash { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    public bool IsAdmin { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public virtual ICollection<ShortenedUrl> ShortenedUrls { get; set; }
+}
